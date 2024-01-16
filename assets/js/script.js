944207439c295e951bc12aee3175463eed41da5c
n4nm4r <nancy.martin.nb@gmail.com>
2024-01-16 09:36:19 -0400

    .

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// Access the div with the id 'output'
+const outputDiv = document.getElementById('output');  
+
+///const means that it gives the variable a permanent global value
+///The 'output, gets my div that i name output
+
+// Log a message to the console
+console.log('Hello from the console!');
+
+// Display the same message on the HTML page
+outputDiv.innerText = 'Hello from the HTML!';
