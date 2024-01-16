399a9e1bb62ef32395ef6ae6df46aaca697e4c51
n4nm4r <nancy.martin.nb@gmail.com>
2024-01-16 10:15:22 -0400

    end of class

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,18 @@ console.log('Hello from the console!');
 
 // Display the same message on the HTML page
 outputDiv.innerText = 'Hello from the HTML!';
+
+///
+
+
+var age = 20;    ///var is another way to create a javascript variable
+
+if (age >= 18) {
+    console.log("You are an adult.");
+} else {
+    console.log("You are a minor.");
+}
+
+// Ternary Example
+var result = (age >= 18) ? "You are an adult." : "You are a minor.";
+console.log(result);
